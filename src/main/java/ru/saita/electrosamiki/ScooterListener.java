5137ecc048dc4c7edc2dcc963eda56b783588cc2
Saita <saita@example.com>
2026-04-23 22:07:58 +0800

    Fix compilation: restore ArmorStand import in ScooterListener

--- src/main/java/ru/saita/electrosamiki/ScooterListener.java
+++ src/main/java/ru/saita/electrosamiki/ScooterListener.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
